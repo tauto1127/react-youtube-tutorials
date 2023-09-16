88010ebc6bea3c9a0fd783a345fe07b03f599995
Takuto <tiharu717@gmail.com>
2023-09-16 17:16:23 +0900

    useStateで選択したアイテムをハイライト表示

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 function ListGroup() {
     let items = [
         'New York',
@@ -7,6 +7,8 @@ function ListGroup() {
         'London',
         'Paris',
     ];
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    //変わる値の時は、stateを使う
 
     const handleClick = (event: MouseEvent) => console.log(event);
     //items = [];
@@ -15,7 +17,9 @@ function ListGroup() {
         {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
             {items.map((item, index) => (
-                <li className="list-group-item" key={item} onClick={handleClick}>{item}</li>//reactはアイテムの変更を検知するため、それぞれの固有のidが必要。
+                <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                 key={item} 
+                 onClick={() => {setSelectedIndex(index)}}>{item}</li>//reactはアイテムの変更を検知するため、それぞれの固有のidが必要。
             ))}
         </ul>
     </>
